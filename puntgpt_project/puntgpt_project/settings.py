203da20392abd90057996a82485a4466bf81b276
developerdesigner18 <debuggerdev.dds@gmail.com>
2025-12-30 16:29:02 +0530

    auth token expiration reduced

--- puntgpt_project/puntgpt_project/settings.py
+++ puntgpt_project/puntgpt_project/settings.py
@@ -167,7 +167,7 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=1200),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "REFRESH_TOKEN_LIFETIME": timedelta(minutes=2),
 }
 
 # email settings
