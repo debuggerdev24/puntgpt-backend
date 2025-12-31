4568d76a5e0495e34f663c261f131c0cc59c1d54
developerdesigner18 <debuggerdev.dds@gmail.com>
2025-12-31 11:11:10 +0530

    token time change

--- puntgpt_project/puntgpt_project/settings.py
+++ puntgpt_project/puntgpt_project/settings.py
@@ -95,6 +95,7 @@ DATABASES = {
         'PASSWORD': os.environ.get('DB_PASSWORD'),
         'HOST': os.environ.get('DB_HOST'),
         'PORT': os.environ.get('DB_PORT'),
+        'TIME_ZONE': 'Australia/Sydney',
     }
 }
 
@@ -167,7 +168,8 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=1),
-    "REFRESH_TOKEN_LIFETIME": timedelta(minutes=1),
+    # "REFRESH_TOKEN_LIFETIME": timedelta(minutes=1),
+    "REFRESH_TOKEN_LIFETIME": timedelta(minutes=1, seconds=30)
 }
 
 # email settings
